2ab9441935529e4f5bc26a08dbc3ca3947cd6935
somoneFromHell <somone_from_hell@tutanota.com>
2024-09-08 17:28:21 +0530

    purifying

--- README.md
+++ README.md
@@ -1,16 +1,5 @@
-hellow dear examinier here is my assignment
+yo its an online game
 
-[screen-recording.webm](https://github.com/user-attachments/assets/8667ada4-04e5-4b1d-88e9-0c60bea30304)
+basicaly you are tank
 
-
-you can checkout the demo of assignment here
-http://mitesh4000.in:3000/
-
-| Endpoint   | Method | Description                                                                                                                                                                            |
-| ---------- | ------ | -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
-| /greetings | GET    | fetch the lattitude and longitude from the ip of http request and send greetings acording to the time zone                                                                             |
-| /wether    | GET    | fetch wether data from database categorise acording to locations and send send formated data in response                                                                               |
-| /locations | POST   | take lattitude and longitude as input and store it in the database and at the time of save it also fetch the wether data of next 5 days of the specific location and store in database |
-
-The project includes a cron job that retrieves weather data from a third-party API and stores the next five days of records for all saved user locations in the database.
-"# invade" 
+you have to blast other tanks
--- src/utils/envVariablesCheck.ts
+++ src/utils/envVariablesCheck.ts
@@ -1,41 +1,18 @@
-import currentTime from "../utils/currentTime";
-const chalk = require("chalk");
+const requiredEnvVars = ["PORT"];
 
 function checkEnvironmentVariables() {
-  if (!process.env.MONGODB_URI) {
-    console.error(`Error: Environment variabel for MONGODB_URI is not set.`);
-    throw new Error(`PORT is required.`);
+  let varNotSet = 0;
+  requiredEnvVars.forEach((envVar) => {
+    if (!process.env[envVar]) {
+      console.error(`ERROR:${envVar} is not set`);
+      varNotSet += 1;
+    }
+  });
+  if (varNotSet > 0) {
+    console.error(
+      `[  ${varNotSet}/${requiredEnvVars.length} variables are not set  ]`
+    );
   }
-
-  if (!process.env.PORT) {
-    console.error(`Error: Environment variabel for PORT is not set.`);
-    throw new Error(`PORT is required.`);
-  }
-
-  if (!process.env.BASE_API_URL) {
-    console.error(`Error: BASE_API_URL is not set.`);
-    throw new Error(`BASE_API_URL is required.`);
-  }
-
-  if (!process.env.CRON_SHEDULE || !process.env.CRON_SHEDULE) {
-    console.error(`Error: CRON_SCHEDULE_IS_NOT_PROVIDED is not set.`);
-    throw new Error(`BASE_API_URL is required.`);
-  }
-
-  if (!process.env.IPINFO_API_KEY) {
-    console.error(`Error: IPINFO_API_KEY is not provided.`);
-    throw new Error(`IPINFO_API_KEY is required.`);
-  }
-  console.log(
-    chalk.yellow(
-      `==================================================  [${chalk.red(
-        currentTime()
-      )}]`
-    )
-  );
-  console.log(
-    chalk.magentaBright("[    Env variables are ok starting server ]")
-  );
 }
 
 export default checkEnvironmentVariables;
